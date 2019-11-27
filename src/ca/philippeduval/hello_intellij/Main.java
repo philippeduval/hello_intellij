7a6a112c94ae1cf021ae30dbe250e38db98baafd
Philippe Duval <philippeduval@mac.com>
2019-11-27 14:45:39 -0500

    commit #2

--- src/ca/philippeduval/hello_intellij/Main.java
+++ src/ca/philippeduval/hello_intellij/Main.java
@@ -1,20 +1,24 @@
 package ca.philippeduval.hello_intellij;
 
 import java.io.PrintStream;
+import java.util.Map;
+import java.util.function.Consumer;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
-        System.out.format("%s %n", "hello world");
+        System.out.format("%s %n", "hello world #2");
 
 
-        System.getProperties().entrySet().stream().forEach(entry -> {
-            System.out.format("-%30s %s %n", entry.getKey(), entry.getValue());
-        });
+        System.getProperties().entrySet().stream().forEach(consumeEntry);
 
 //        System.getProperty("java.version")
 
     }
+
+    static Consumer<Map.Entry> consumeEntry = entry ->
+            System.out.format("-%30s %s %n", entry.getKey(), entry.getValue());
+
 }
